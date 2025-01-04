42a1d3d6f8dbd5567e4738da6a9437a77eb46682
Abishek Kafle <72290474+abishek-kafle@users.noreply.github.com>
2025-01-04 19:42:46 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "abi.kaf@gmail.com"
+// Try not to use var
+var accountPassword = "12345"
+// Bad practice
+accountCity = "Connecticut" 
+let accountState;
+
+accountId = 2
+
+console.log(accountId);
+/*
+Multi line comment
+Prefer not to use var because of issue in block scope and functional scope.
+*/
