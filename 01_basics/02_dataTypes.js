9fdbaf73791de54d978433a1ca6de636cb26d12e
Abishek Kafle <72290474+abishek-kafle@users.noreply.github.com>
2025-01-05 13:58:00 +0000

    conversion of data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,36 @@
 
 // alert( 3 + 3 ) // we are using nodejs, not browser
 
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(boolIsLoggedIn);
+// console.log(typeof boolIsLoggedIn);
+
+let num = 33
+
+let stringNumber = String(num)
+
+console.log(typeof num);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
